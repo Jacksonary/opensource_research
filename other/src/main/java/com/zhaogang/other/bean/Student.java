c7d469e4e7b77052bd91db78ba7f29adde5c5e1b
Jacky Liu <jacksonary@163.com>
2022-01-07 10:12:58 +0800

    sth

--- other/src/main/java/com/zhaogang/other/bean/Student.java
+++ other/src/main/java/com/zhaogang/other/bean/Student.java
@@ -8,9 +8,21 @@ import lombok.Data;
  * @description
  */
 @Data
-public class Student {
+public class Student implements Cloneable {
 
     private Integer id;
     private String name;
     private Integer age;
+    private Clazz clazz;
+
+    @Override
+    public Student clone() {
+        try {
+            Student clone = (Student)super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
---
+++ other/src/main/java/com/zhaogang/other/test/Main.java
@@ -0,0 +1,8 @@
+package com.zhaogang.other.test;
+
+/**
+ * @author jacks
+ * @date 2021/12/30
+ */
+public class Main {
+}
